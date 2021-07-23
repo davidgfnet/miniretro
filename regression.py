fa65293103a489f1167597c7330d6a06308effab
David Guillen Fandos <david@davidgf.net>
2021-07-23 19:54:56 +0200

    Add new report based on templates

--- regression.py
+++ regression.py
@@ -18,6 +18,7 @@ parser.add_argument('--core', dest='core', required=True, help='Core (.so file) 
 parser.add_argument('--system', dest='system', required=True, help='system directoy')
 parser.add_argument('--driver', dest='driver', type=str, default="./miniretro", help='Miniretro driver commandline')
 parser.add_argument('--frames', dest='frames', type=int, default=3200, help='Frames per game to run')
+parser.add_argument('--capture', dest='capture', type=int, default=1, help='Number of frames to capture')
 parser.add_argument('--threads', dest='threads', type=int, default=8, help='CPUs (threads) to use')
 parser.add_argument('--input', dest='infiles', nargs='+', help='Set of files or directories to use as test files')
 parser.add_argument('--output', dest='output', required=True, help='Output report file (either .txt or .html)')
@@ -34,7 +35,10 @@ for elem in args.infiles:
 
 os.mkdir(args.output)
 # Fake controls :)
-ctrl = ["%d:a %d:a %d:start %d:start" % (i, i+30, i+60, i+90) for i in range(0, args.frames, 300)]
+ctrl = ["%d:a %d:a %d:a %d:a %d:start %d:start %d:start %d:start" % (
+   i, i+1, i+2, i+30, i+60, i+90, i+91, i+92) for i in range(0, args.frames, 300)]
+# Take the last frame and N-1 frames from start too
+frameevery = args.frames // args.capture
 
 def runcore(rom):
   romid = hashlib.sha1(open(rom, "rb").read()).hexdigest()[:12]
@@ -50,9 +54,10 @@ def runcore(rom):
          "--output", opath,
          "--system", args.system,
          "--input", " ".join(ctrl),
-         "--dump-frames-every", str(args.frames),
+         "--dump-frames-every", str(frameevery),
          "--frames", str(args.frames)],
-        stdout=stdout, stderr=stderr)
+        stdout=stdout, stderr=stderr,
+        preexec_fn=lambda : os.nice(10))
       spcall.wait()
   with open(os.path.join(opath, "results.json"), "w") as metafd:
     metafd.write(json.dumps({
